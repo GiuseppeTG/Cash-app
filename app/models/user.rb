dc2fc11b7ed9a67442ebb1c19129569f4259e376
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-10-07 13:35:39 -0500

    Generate diagram structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :photo, presence: true
+  validates :role, presence: true
+
+  has_many :expenses, dependent: :destroy
+  has_many :groups, dependent: :destroy
 end
--- db/migrate/20221007162230_create_users.rb
+++ db/migrate/20221007162230_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name, null: false
+      t.string :role, null: false, default: 'user'
+      t.string :photo, null: false, default: 'https://www.kindpng.com/picc/m/22-223965_no-profile-picture-icon-circle-member-icon-png.png'
 
       t.timestamps
     end
