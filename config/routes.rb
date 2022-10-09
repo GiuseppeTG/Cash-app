90064cf2ebbded606ebec91f2d432c33750dca4e
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-10-09 17:14:11 -0500

    Set up navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'groups#index'
   devise_for :users
   resources :groups
   resources :expenses
-  root 'users#index'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
